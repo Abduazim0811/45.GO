1ac4e06bb9a210b67ef265691372f6000b43d103
Abduazim <yusupovabduazim0811@gmail.com>
2024-06-03 11:19:03 +0500

    first

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	// 1. Go arxivini yuklab olish
 	goURL := "https://go.dev/dl/go1.22.3.src.tar.gz"
 	archivePath := "go1.22.3.src.tar.gz"
 	err := heshlash.DownloadFile(goURL, archivePath)
@@ -15,7 +14,6 @@ func main() {
 		return
 	}
 
-	// 2. JSON ma'lumotidan kerakli SHA-256 hashini olish
 	jsonURL := "https://go.dev/dl/?mode=json"
 	expectedHash, err := heshlash.GetExpectedHash(jsonURL)
 	if err != nil {
@@ -23,14 +21,12 @@ func main() {
 		return
 	}
 
-	// 3. Yuklab olingan arxivning SHA-256 hashini hisoblash va solishtirish
 	calculatedHash, err := heshlash.CalculateSHA256(archivePath)
 	if err != nil {
 		fmt.Printf("Error calculating hash: %v\n", err)
 		return
 	}
 
-	// Hashlarni solishtirish
 	if calculatedHash == expectedHash {
 		fmt.Println("true")
 	} else {
